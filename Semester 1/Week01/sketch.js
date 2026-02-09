fc370713405e01fe472e70080db0b5fff983b020
SwhUniAcc <samuel2.williams-hicks@live.uwe.ac.uk>
2026-02-09 15:58:28 +0000

    G&WDS Merge
    Merged all G&WDS repositories together to be less confusing

---
+++ Semester 1/Week01/sketch.js
@@ -0,0 +1,11 @@
+// creating a background
+function setup() {
+createCanvas(1080, 720);
+background(255);
+//noLoop();
+}
+
+// display stuff on the background
+function draw() {
+text("Hey all! Scott here and DAMNIT!!! i own Sonic Jam now!", 540, 360);
+}
